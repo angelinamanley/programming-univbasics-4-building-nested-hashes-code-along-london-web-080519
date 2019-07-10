54dae30e9afbc8d7d4ea02a4aede4bc8e096cd84
Angelina Shin Manley <ashin05@gmail.com>
2019-07-10 14:12:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
       :matriarch => { name: "Lady Capulet", age:  "51"
         #your key/value pairs here
         },
-      :heroine => { name: "Juliet", age: 15, status: "alive"
+      :heroine => { name: "Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"}]
